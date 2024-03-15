6f2523dd522f2336b32f32d2853c38527c1236c5
Albert T <albert.terrazas@gmail.com>
2024-03-15 11:44:43 -0500

    fixed bug

--- routes/route-API.js
+++ routes/route-API.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const router = express.Router(); // Import and create an instance of Router
 const { v4: uuidv4 } = require('uuid');
-const router = express.Router(); // Import Router from express
+const fs = require("fs");
 
-router.get('/api/notes', async (req, res) => {
-  const dbJson = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
-  res.json(dbJson);
-  // Your route handling logic
+router.get('/api/notes', (req, res) => {
+  try {
+    const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    res.json(dbJson);
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 router.post('/api/notes', (req, res) => {
@@ -24,9 +29,10 @@ router.delete('/api/notes/:id', (req, res) => {
   let dNote = fs.readFileSync("db/db.json', 'utf8'");
   const dataJSON =  JSON.parse(dNote);
   const jots = dataJSON.filter((note) => { 
-    return note.id!== req.params.id;
+    return note.id !== req.params.id;
   });
   fs.writeFileSync("db/db.json",JSON.stringify(jots));
   res.json("Deleted notes.");
 });
+
 module.exports = router;
--- routes/routes-Html.js
+++ routes/routes-Html.js
@@ -2,11 +2,11 @@ const htmlRouter = require('express').Router();
 const path = require('path');
 
 htmlRouter.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')); 
+    res.sendFile(path.join(__dirname, '../public/index.html')); 
 });
 
 htmlRouter.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'pages', 'notes.html')); 
+    res.sendFile(path.join(__dirname, '../public/notes.html')); 
 });
 
 module.exports = htmlRouter;
